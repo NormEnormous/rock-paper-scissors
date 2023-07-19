c4433f46a5411b8430fb9c8effd84b8c81861ff4
NormEnormous <130330451+NormEnormous@users.noreply.github.com>
2023-07-18 20:07:30 -0400

    Pseudo code

---
+++ game.js
@@ -0,0 +1,4 @@
+//   getComputerChoice, randomize the variable 1-3.
+//   getPlayerChoice, randomize and return in a lowercase. case to variable.
+//   playRound() function, takes getComputerChoice, getPlayerChoice.
+//   game()Function repeating single round, for loop until 5.
