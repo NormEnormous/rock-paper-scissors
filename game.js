e2e66dd9eb52831a0304c4ae70ca1a896604f431
NormEnormous <130330451+NormEnormous@users.noreply.github.com>
2023-07-18 20:28:54 -0400

    Computer input, get player input, play round

--- game.js
+++ game.js
@@ -1,4 +1,46 @@
 //   getComputerChoice, randomize the variable 1-3.
+
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random() * 3);
+    return computerChoice;
+}
+
+getComputerChoice();
+
 //   getPlayerChoice, randomize and return in a lowercase. case to variable.
+
+function getPlayerChoice() {
+    let playerChoice = prompt("Rock, Paper, Scissors?", "").toLowerCase();
+    return playerChoice;
+}
+
 //   playRound() function, takes getComputerChoice, getPlayerChoice.
+
+function playRound() {
+    let computerChoice = getComputerChoice();
+    let playerChoice = getPlayerChoice();
+    let result = computerChoice + playerChoice; {
+        console.log(result);
+        switch (result) {
+            case '01':
+            case '12':
+            case '30':
+                console.log("You Win!");
+                break;
+            case '10':
+            case '21':
+            case '03':
+                console.log("You Lose!");
+                break;
+            default: "It's a tie!"
+        }
+    }
+}
+playRound();
+
+
+
 //   game()Function repeating single round, for loop until 5.
+
+
+
